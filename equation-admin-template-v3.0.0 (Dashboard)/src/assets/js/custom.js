5e63965e4f28dfeb760e1c5d8ab28fcbaaed08ec
muhammadyounus-a <muhammadyounus6124@gmail.com>
2025-02-04 14:28:20 +0530

    Maintenance

--- equation-admin-template-v3.0.0 (Dashboard)/src/assets/js/custom.js
+++ equation-admin-template-v3.0.0 (Dashboard)/src/assets/js/custom.js
@@ -126,7 +126,7 @@ function quickBtn() {
 // backBtn1
 
 function backBtn1() {
-    let showCards = document.querySelectorAll(".show-card-2, .revenue-card, .outStanding-card, .profit-loss-card, .branch-card, .utilization-card");
+    let showCards = document.querySelectorAll(".show-card-2, .revenue-card, .outStanding-card, .profit-loss-card, .branch-card, .utilization-card, .maintenance-card");
     let quickCard = document.querySelectorAll(".quick-card, .show-card-1");
     showCards.forEach(card => {
         card.style.display = "none";
@@ -223,6 +223,27 @@ function branchBtn() {
 // utilization code
 function utilizationBtn() {
     let showCards = document.querySelectorAll(".utilization-card, .show-card-1, .show-card-2");
+
+    showCards.forEach(card => {
+        if (card.classList.contains("show-card-1")) {
+            card.style.display = "none";
+        } else if (card.classList.contains("show-card-2")) {
+            card.style.display = "block";
+            card.style.display = "flex";
+        } else {
+            card.style.display = "block";
+        }
+    });
+
+    let quickCard = document.querySelectorAll(".quick-card");
+    quickCard.forEach(card => {
+        card.style.display = (card.style.display === "none" || card.style.display === "") ? "block" : "none";
+    });
+}
+
+// maintenance code
+function maintenanceBtn() {
+    let showCards = document.querySelectorAll(".maintenance-card, .show-card-1, .show-card-2");
 
     showCards.forEach(card => {
         if (card.classList.contains("show-card-1")) {
